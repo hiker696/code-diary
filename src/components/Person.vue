e05c071d85ebb72fa779ae89f894e9d5f2f5a858
hiker696 <hujiaying2004@gmail.com>
2025-07-29 23:02:50 +0800

    setup语法糖，ref，reactive

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,57 +1,54 @@
-<template>
-    <div class="person">
-        <h2>姓名：{{  name }}</h2>
-        <h2>nianlinig:{{  age }}</h2>
-        <button @click="changeName">修改名字</button>
-        <button @click="changeAge">修改年龄</button>
-        <button @click="showTel">查看练习方式</button>
-    </div>
-</template>
+<script setup>
+import { ref, reactive } from 'vue'
 
-<script lang=" ts">
-export default{
-    name:'Person',
-    setup(){
-        //数据,原来是写在data，此时name,age,tel都不是响应式的
-        let name = '张三';//注意此时的name不是响应式的
-        let age = 18;//响应式数据需要使用ref或reactive
-        let tel = '1234567890'//
+const form = reactive({
+    name: '',
+    age: null
+})
 
-        //方法
-        function changeName() {
-            name = '李四',
-            console.log(name);
-        }
-        function changeAge() {
-            age += 1,
-        console.log(age);
-        }
-        function showTel() {
-            alert(tel)
-            console.log(tel);
-        }
+const likes = ref(0)
 
-        // return{
-            // name,
-            // age,
-            // changeAge,
-            // changeName,
-            // showTel
-        // }
-        return ()=>'hahha'
-    }
+function submitFrom() {
+    alert(`submitted: ${form.name},age ${form.age}`)
 }
 </script>
 
+<template>
+<div class="container">
+<h2>Registration From</h2>
+<label>
+    name:
+    <input type="text" v-model="form.name"/>
+</label>
+<label >
+    age:
+    <input type="number" v-model="form.age"/>
+    </label>
+    <button @click="submitForm">submit</button>
+
+    <p>form data:</p>
+    <pre>{{ form }}</pre>
+
+    <hr />
+
+    <h2>like counter</h2>
+    <button @click="likes++">like {{ likes }}</button>
+
+</div>
+</template>
+
 <style scoped>
-.person {
-    /* Add your styles here */
-    background-color: skyblue;
-    padding: 20px;
-    border-radius: 5px;
-    font-family: Arial, sans-serif;
+.container{
+    max-width: 400px;
+    margin:auto;
+    font-family: Arial, Helvetica, sans-serif;
 }
-button{
-    margin:0 5px;
+
+input{
+    display: block;
+    margin:10px 0;
+    padding: 5px;
+    width: 100%;
+    box-sizing: border-box;
 }
 </style>
